7a88df5fc07f7cf0ce376a7faa8406ce32b86e01
Ryu <ryupol@hotmail.com>
2024-04-21 21:24:55 +0700

    try to deploy

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ import commentRouter from "./api/routes/comments.routes.js";
 dotenv.config();
 
 const app = express();
+PORT = 3000;
 
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
@@ -39,6 +40,6 @@ app.use("/api/comments", commentRouter);
 app.use("/images/thumbnail", express.static("assets/images/thumbnail"));
 app.use("/images/profile", express.static("assets/images/profile"));
 
-app.listen(3000, () => {
-  console.log("Server started on port http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on port ${PORT}`);
 });
