0a640adcd480054313ab3acacdb63793215848f6
Ryu <ryupol@hotmail.com>
2024-04-21 21:43:50 +0700

    change vite config for deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://my-youtube-api.vercel.app",
         changeOrigin: true,
       },
     },
